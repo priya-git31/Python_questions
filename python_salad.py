53061ea824cdafd58e4cf160fb7c437a3f657b3f
priya-git31 <haripriyagouddr@gmail.com>
2024-11-04 17:43:13 -0500

    solutions uodate

--- python_salad.py
+++ python_salad.py
@@ -21,35 +21,35 @@ print(result2)
 
 
 
-# # 3 numbers = 1234321 - check if the given number is palindrome
-# def palindrome(given_numbers): 
-#    if given_numbers[::-1] == given_numbers:
-#      return ("It is a Palindrome")
-#    else: 
-#      return ("It is not a Palindrome")
+# 3 numbers = 1234321 - check if the given number is palindrome
+def palindrome(given_numbers): 
+   if given_numbers[::-1] == given_numbers:
+     return ("It is a Palindrome")
+   else: 
+     return ("It is not a Palindrome")
 
-# result = palindrome("1234321")
-# print(result)
+result = palindrome("1234321")
+print(result)
 
-# result2 = palindrome("1234325")
-# print(result2)
+result2 = palindrome("1234325")
+print(result2)
 
 
-# #4 find those pair of numbers such that their sum is divisible by 2
-# numbers_list = [(10,22), (20, -15), (8,8), (1,2)]
-# result = [(x,y) for (x,y) in numbers_list if (x + y ) % 2 == 0 ]
-# print(result)
+#4 find those pair of numbers such that their sum is divisible by 2
+numbers_list = [(10,22), (20, -15), (8,8), (1,2)]
+result = [(x,y) for (x,y) in numbers_list if (x + y ) % 2 == 0 ]
+print(result)
 
-# #5 find those pair of numbers such that their sum is divisible by 3
-# odd_list = [(10,22), (20, -15), (8,8), (1,2)]
-# result = [(x,y) for (x,y) in numbers_list if (x + y ) % 3 == 0 ]
-# print(result)
+#5 find those pair of numbers such that their sum is divisible by 3
+odd_list = [(10,22), (20, -15), (8,8), (1,2)]
+result = [(x,y) for (x,y) in numbers_list if (x + y ) % 3 == 0 ]
+print(result)
 
-# #6 Use list comprehensions to find the square and cubes of numbers that are 
-# # between 10 and 50 and also that are multiples of 3
+#6 Use list comprehensions to find the square and cubes of numbers that are 
+# between 10 and 50 and also that are multiples of 3
 
-# import math
-# numbers = [ x for x in range(1,51) if x % 3 == 0]
-# print(numbers)
-# result = [(math.sqrt(x), pow(x,3)) for x in numbers]
-# print(result)
+import math
+numbers = [ x for x in range(1,52) if x % 3 == 0]
+print(numbers)
+result = [(math.sqrt(x), pow(x,3)) for x in numbers]
+print(result)
