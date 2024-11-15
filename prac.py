9981e6a46402394f040d79ace64604dd5934a768
priya-git31 <haripriyagouddr@gmail.com>
2024-11-15 15:30:35 -0500

    solutions updated

--- prac.py
+++ prac.py
@@ -20,7 +20,7 @@ else:
     print("odd number")
 
 #2Python Program to Check Whether a Number is Positive or Negative
-number = -17
+number = 17
 
 if number > 0: 
     print("positive number")
@@ -28,44 +28,41 @@ else:
     number < 0
     print("negative number")
 
+#3Python Program to Print All Odd Numbers in a Range
 
-# # Python Program to Reverse a Number
+for i in range(1,11): 
+    if i % 2 != 0: 
+        print(i)
 
-# number = 1221
+#4Python Program to Check if a Number is a Palindrome
 
-# def reverse_number(given_number): 
-#     string_number = str(given_number)
-#     if string_number == string_number[::-1]: 
-#         return "palindrome"
-#     else: 
-#         return "not palindrome"
+number = 1221
+
+def palindrome(given_number): 
+    string_number = str(given_number)
+    if string_number == string_number[::-1]: 
+        return "palindrome"
+    else: 
+        return "not palindrome"
     
-# result = reverse_number(number)
-# print(result)
+result = palindrome(number)
+print(result)
 
+#5Python Program to Reverse a Number
 
+number = 1290
 
-# # Python Program to Print All Integers that Aren’t Divisible by Either 2 or 3
+def reverse_number(number): 
+    str_number = str(number)
+    result = str_number[::-1]
+    return result 
 
-# for i in range(1 11): 
-#     if i % 2 != 0 and i % 3 != 0: 
-#         print(i)
+result = reverse_number(number)
+print(result)
 
+# 6 Python Program to Print All Integers that Aren’t Divisible by Either 2 or 3
 
-# # # Python Program to Print All Numbers in a Range Divisible by a Given Number
-# number = 7 
+for i in range(1,11): 
+    if i % 2 != 0 and i % 3 != 0: 
+        print(i)
 
-# for i in range (1,16): 
-#     if i % number == 0: 
-#         print(i)
-
-# # Python Program to Find Sum of Digits of a Number
-
-# number = 123 
-
-# def sum_of_digits(number): 
-#     con_list = list(number)
-#     return con_list 
-
-# result = sum_of_digits(number)
-# print(number)
