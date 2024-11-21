4f888e7f933a4a4e3897cc8c3a0b1183b44a96c6
priya-git31 <haripriyagouddr@gmail.com>
2024-11-21 14:45:23 -0500

    strings updated

--- sunfoundry.strings.py
+++ sunfoundry.strings.py
@@ -1,5 +1,5 @@
 
-# 4 Python Program to Replace All Occurrences of ‘a’ with $ in a String
+# # 4 Python Program to Replace All Occurrences of ‘a’ with $ in a String
 # string_input = input("Enter a string: ")
 
 # def replace_string(given_string,existing,replaced): 
@@ -10,23 +10,78 @@
 # print(result)   
 
 
-# # Python Program to Remove the nth Index Character from a Non-Empty String
+# # 5 Python Program to Replace Every Blank Space with Hyphen in a String
 # string_input = input("Enter a string: ")
-# srting_index_remove = int(input("Enter the index: "))
 
-# def remove_string_index(given_string,index_num): 
-#     removed_string = given_string.pop[2]
-#     return given_string 
+# def replace_string(given_string,existing,replaced): 
+#     result = given_string.replace(existing, replaced)
+#     return result
 
-# result = remove_string_index(string_input,srting_index_remove)
+# result = replace_string(string_input, " ", "-")
+# print(result) 
+
+
+# 2 Python Program to Remove Odd Indexed Characters in a string
+# string_temp = "Acotara"
+# print(string_temp[0::2])
+
+
+# 17 Python Program to Check if the Substring is Present in the Given String
+# string_input = input("Enter a string: ")
+# string_substring = input("Enter a word: ")
+
+# def check_substring(string_input,string_substring): 
+#     if string_substring in string_input: 
+#         return "True"
+#     else: 
+#         return "False"
+    
+# result = check_substring(string_input,string_substring)
 # print(result)
 
-# Python Program to Replace Every Blank Space with Hyphen in a String
-string_input = input("Enter a string: ")
 
-def replace_string(given_string,existing,replaced): 
-    result = given_string.replace(existing, replaced)
-    return result
+# 22 Python Program to Create a New String Made up of First and Last 2 Characters
+# original_string = input("Enter a word: ")
 
-result = replace_string(string_input, " ", "-")
-print(result) 
+# def creating_new(original_string): 
+#     new_string = original_string[0] + original_string[-2:]
+#     return new_string 
+
+# result = creating_new(original_string)
+# print(result)
+
+# 12 Python Program to Count the Number of Vowels in a String
+
+# given_string = "Facebook"
+
+# def count_vowels(given_string): 
+#     empty_string = 0
+#     for i in given_string: 
+#             if i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u':
+#                  empty_string = empty_string + 1
+#     return empty_string 
+
+# result = count_vowels(given_string)
+# print(result)
+
+#LOOPS 
+# given_string = input("Enter a word: ")
+
+# empty_string = 0
+
+# for i in given_string: 
+#     if i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u':
+#         empty_string = empty_string + 1
+# print(empty_string)
+
+
+# # 11 Python Program to Count Number of Lowercase Characters in a String
+
+given_string = input("Enter a word: ")
+
+empty_string = 0 
+
+for i in given_string: 
+    if i == i.lower():
+        empty_string = empty_string + 1
+print(empty_string)
