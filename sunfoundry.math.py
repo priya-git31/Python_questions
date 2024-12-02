4165a8913637d583d29e7990f22cac9404cef293
priya-git31 <haripriyagouddr@gmail.com>
2024-12-02 15:10:08 -0500

    Sunfoundry questions

---
+++ sunfoundry.math.py
@@ -0,0 +1,19 @@
+# Python Program to Find the Area of a Triangle
+
+# import math
+
+# a=int(input("enter side 1: "))
+# b=int(input("enter side 2: "))
+# c=int(input("enter side 3: "))
+# s= (a + b + c )/2
+# area_triangle = math.sqrt(s * (s-a) * (s - b) * (s - c))
+# print(f"The area of triangle is {area_triangle}")
+
+# Python Program to Find Simple Interest
+# P = int(input("Enter a principal amount: "))
+# IR = float(input("Enter a Interest rate : "))
+# TP = int(input("Enter the time period(years): "))
+# SI = (P * IR * TP)/100
+# print(SI)
+
+# Python Program to Find All Pythagorean Triplets in the Range
